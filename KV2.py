cfc127d0d1a252b361e56d7159e283ee7d7549df
suakari2 <53440544+suakari2@users.noreply.github.com>
2019-07-31 10:10:35 -0400

    Add files via upload

---
+++ KV2.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 30 10:50:29 2019
+
+@author: STEM
+"""
+
+def cadbury(a):
+    cadbury = a+10
+    
+    print(cadbury)
+    
+cadbury(5)
+    
+def cadbury(a,b):
+    
+    print(a+b)
+    
+cadbury(15,5)
+
+def cadburyBox(c1,c2,c3):
+    
+    print ("There are", c1, "milk chocolates,",c2, "dark chocolates, and",c3, "white chocolates.")
+cadburyBox(5,5,5)
+
+a="Karo"
+print("Hello",a)   
+
+name = input("enter your name")
+
+print("your name is",name )
+
+age = input("age")
+
+print("you entered", age)
+
+ageint = int(age)
+ageint
+
+ageint(float)
+
+import math
+
+dir(math)
+
+math.pi
+
+math.factorial(0)
+
+math.exp(15)
+
+math.pow(.6,15)
+
+math.sin(math.pi/2)
+
+math.pow(8,1/3)
+
+import math
+a= int(input("enter a value"))
+def cube(a):
+    print("The cube root of", a, "is", math.pow(a,(1/3))
+    
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ KVVY.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 29 11:04:45 2019
+
+@author: STEM
+"""
+
+Hello my name is Karolina
+
+print("Hello my name is Karolina")
+prnt("kari")
+5*2
+5/2
+5-2
+5**2
+(8/9)*3
+
+print("5*2")
+
+def multiply(k,v):
+    multiply = k*v
+    
+    print(multiply)
+    
+multiply(4,5)
+
+def add(q,r,s):
+    add=q+r+s
+    
+    print(add)
+    
+add(2,3,4)
+
+def subtract(t,r):
+    subtract = t-r
+    
+    print(subtract)
+    
+subtract (6,2)
+
+def area(b,h):
+    area = b*h/2
+    
+    print("The area of the triangle with base", b , "and height", h , "is", area)  
+    
+area(2,4)
